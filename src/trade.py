5bb28116dd988c8f94fbcf5464358cc7be354fd5
Max <max.nibler@gmail.com>
2020-12-20 17:39:20 -0800

    Keys loaded

--- src/trade.py
+++ src/trade.py
@@ -4,12 +4,20 @@
 
 #Lib imports
 import sys
+import json
 
 #Local imports
 from stock import Stock
 import mylogging as mylog
 
 __KEYFILE__ = '.alpacakeys.txt'
+
+def connect():
+  keyfile = open(__KEYFILE__, 'r')
+  keys = json.load(keys)
+  print(keys)
+  keyfile.close()
+  return
 
 def buy(inStock):
   mylog.baseLog("Buy {n}, Last: {l}, MA: {ma}"
